4bf037ca3a32467f13b48c1b073306843958a9d6
Mustapha Garba <mgarba88@gmail.com>
2024-11-04 17:15:17 +0100

    closing from work for the day

--- manager.py
+++ manager.py
@@ -1,3 +1,15 @@
 pwd = input("What is the master password? : ")
 
-mode = input("Would you like to add a new password?")
+def view_password():
+    pass
+def add_password():
+    pass
+
+while True:
+    mode = input("Would you like to add a new password or view existing passwords (add, view)? :" )
+    if mode == 'view' or mode == 'View':
+        pass
+    elif mode == 'add' or mode == 'Add':
+        pass
+    else:
+        print('Invalid selection')
